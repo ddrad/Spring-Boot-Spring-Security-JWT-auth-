a07a7df86af1a0f60e19281be2b807f0b657ec74
ddrad <mr.azaroff@gmail.com>
2018-09-21 13:32:51 +0200

    Modified BusinessAccountProcessing

--- x3-bpm-app/src/main/java/org/camunda/bpm/getstarted/loanapproval/process/BusinessAccountProcessing.java
+++ x3-bpm-app/src/main/java/org/camunda/bpm/getstarted/loanapproval/process/BusinessAccountProcessing.java
@@ -2,14 +2,10 @@ package org.camunda.bpm.getstarted.loanapproval.process;
 
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.getstarted.loanapproval.model.ConsumerRequest;
-import org.camunda.bpm.getstarted.loanapproval.model.ConsumerTarget;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.LinkedHashMap;
-
 
 @Component
 public class BusinessAccountProcessing {
